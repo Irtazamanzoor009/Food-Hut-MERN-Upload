0c5f9851e14e553be5884b32fde7333b64596c4d
Irtaza Manzoor <irtaza.manzoor1203@gmail.com>
2024-07-13 22:58:41 +0500

    update

--- backend/routes/displaydata.js
+++ backend/routes/displaydata.js
@@ -3,11 +3,12 @@ const router = express.Router();
 
 router.post("/fooditems", (req, res) => {
   try {
-    res.json([global.food_items, global.food_category])
-    res.send("Data Fetched or not")
+    res.send([global.food_items, global.food_category])
+    res.json({ success: false })
   } catch (error) {
     console.log(error);
     res.status(500).send("Server Error");
+    res.json({ success: false });
   }
 });
 
