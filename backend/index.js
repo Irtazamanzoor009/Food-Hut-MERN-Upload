8364f0dc23b305d193003c98cbc7bf092d824818
Irtaza Manzoor <irtaza.manzoor1203@gmail.com>
2024-07-13 20:17:33 +0500

    ad

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,10 @@ connection_db();
 
 
 const app = express()
-const port = 3001
+const port = 3002
 
 const corsOptions = {
-  origin: ['http://localhost:3000', 'https://https://food-hut-mern.vercel.app/'], // Replace with your Vercel domain
+  origin: ['http://localhost:3002', 'https://https://food-hut-mern.vercel.app/'], // Replace with your Vercel domain
   optionsSuccessStatus: 200,
 };
 
